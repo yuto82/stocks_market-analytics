98da6fe0b61c61b6ea311bfdb07303f948b434de
yuto82 <rgritsal@gmail.com>
2025-08-04 00:12:26 +0200

    add: logger

--- src/analytical_pipeline/config/settings.py
+++ src/analytical_pipeline/config/settings.py
@@ -4,6 +4,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    # Polygon.io
     POLYGON_API_KEY = os.getenv("POLYGON_API_KEY", "")
     POLYGON_BASE_URL = os.getenv("POLYGON_BASE_URL", "https://api.polygon.io")
-    
+
+    # Kafka
+    KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+    KAFKA_TOPICS = {
+        "trades": "stock-trades",
+        "quotes": "stock-quotes",
+        "aggregates": "stock-aggregates"
+    }
+
+    # Logging
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+    @classmethod
+    def validate_required_env_variables(cls):
+        required_variables = [
+            "POLYGON_API_KEY",
+        ]
+
+        missing_variables = []
+
+        for variable in required_variables:
+            if not getattr(cls, variable):
+                missing_variables.append(variable)
+        
+        if missing_variables:
+            raise ValueError(f"Missing variables: {missing_variables}")
+        
+        return True
---
+++ src/analytical_pipeline/utils/logger.py
@@ -0,0 +1,60 @@
+import sys
+import logging
+from pathlib import Path
+from typing import Optional
+from ..config.settings import Config
+from colorlog import ColoredFormatter
+
+def setup_logger(name: str, level: Optional[str] = None, enable_file_logging: Optional[bool] = True) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger_level = level or Config.LOG_LEVEL
+
+    try:
+        logger.setLevel(getattr(logging, logger_level.upper()))
+    except AttributeError:
+        logger.setLevel(logging.INFO)
+        logger.warning(f"Warning: Unknown log level '{logger_level}', using INFO level.")
+
+    logger.handlers.clear()
+
+    console_formatter = ColoredFormatter(
+        fmt = "%(log_color)s[%(asctime)s] - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+            }
+        )
+    
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    if enable_file_logging:
+        log_path = Path("logs")
+        log_path.mkdir(exist_ok=True)
+        log_file = log_path / f"{name}.log"
+
+        file_formatter = logging.Formatter(
+            fmt="[%(asctime)s] - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+
+        try:
+            file_handler = logging.FileHandler(log_file, encoding="utf-8")
+            file_handler.setFormatter(file_formatter)
+            logger.addHandler(file_handler)
+        except Exception as e:
+            logger.warning(f"Cannot create log file {log_file}: {e}.")
+    
+    logger.propagate = False
+
+    return logger
+
+polygon_logger = setup_logger("polygon_producer")
+kafka_logger = setup_logger("kafka_consumer")
+spark_logger = setup_logger("spark_processor")
+snowflake_logger = setup_logger("snowflake_connector")
